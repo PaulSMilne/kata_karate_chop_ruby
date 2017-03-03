75a34fdb1f449e95b9272d4112c19b1eeb6ca958
Paul Milne <hallhill@gmail.com>
2017-03-03 15:42:07 +0000

    setting up some case statements in the first binary search algorithm

--- karate_chop1.rb
+++ karate_chop1.rb
@@ -1,6 +1,19 @@
 class KarateChop
 
-   def chop(num, array)
-   end
+    def chop(num, array)
+        chop_length = array.length
+        case chop_length
+            when 0
+                return -1
+            when 1
+                if num == array[0]
+                    return num
+                else
+                    return -1
+                end
+            else
+
+        end
+    end
 
 end
